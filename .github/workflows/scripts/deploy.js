24c1efdf98a4e16f6fcb811b81e5a4a210045447
devevandro <dev.evandro@gmail.com>
2025-02-02 22:49:22 -0300

    fix: comment out unused environment variable checks in deploy.js

--- .github/workflows/scripts/deploy.js
+++ .github/workflows/scripts/deploy.js
@@ -3,15 +3,15 @@ const core = require('@actions/core');
 
 module.exports = async ({ github, context, core }) => {
   try {
-    const coolifyUrl = process.env.COOLIFY_URL;
-    const coolifyToken = process.env.COOLIFY_TOKEN;
-    const appId = process.env.APP_UUID;
-    const secrets = process.env.SECRETS;
+    // const coolifyUrl = process.env.COOLIFY_URL;
+    // const coolifyToken = process.env.COOLIFY_TOKEN;
+    // const appId = process.env.APP_UUID;
+    const secrets = core.getInput('secrets', { required: true });
     console.log('secrets', secrets);
 
-    if (!coolifyUrl || !coolifyToken || !appId) {
-      throw new Error('Missing required environment variables');
-    }
+    // if (!coolifyUrl || !coolifyToken || !appId) {
+    //   throw new Error('Missing required environment variables');
+    // }
 
     const api = axios.create({
       baseURL: coolifyUrl,
